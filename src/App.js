4fafbf96479dced503abb77f041394c268265c25
JuanFRey <fernan2lopezkto@gmail.com>
2023-09-18 09:27:10 -0300

    anu simple changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Componentes/Header';
+import NavBar from './Componentes/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Header />
     </div>
   );
