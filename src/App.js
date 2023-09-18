9d50141600318f012d5534de5276b4b37f90f8ae
JuanFRey <fernan2lopezkto@gmail.com>
2023-09-18 09:30:36 -0300

    anu simple changes 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Componentes/Header';
 import NavBar from './Componentes/NavBar';
+import VideoPlayer from './Componentes/vid';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
+      <VideoPlayer />
     </div>
   );
 }
